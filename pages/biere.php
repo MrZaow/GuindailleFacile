7de8aab93a3f36ebf4cf8baf5f8925fe00e4d107
Fllotouch <Floca@MacBook-Air-de-Florent.local>
2015-02-12 17:48:19 +0100

    Correction html

--- pages/biere.php
+++ pages/biere.php
@@ -259,106 +259,16 @@
 
 		</div>
 		<!-- End content -->
-
-
-		<!-- footer 
-			================================================== -->
-		<footer>
-			<div class="social-section">
-				<ul class="social-icons triggerAnimation animated" data-animate="tada">
-					<li><a class="facebook" href="portfolio-4col.html#"><i class="fa fa-facebook"></i></a></li>
-					<li><a class="twitter" href="portfolio-4col.html#"><i class="fa fa-twitter"></i></a></li>
-					<li><a class="rss" href="portfolio-4col.html#"><i class="fa fa-rss"></i></a></li>
-					<li><a class="google" href="portfolio-4col.html#"><i class="fa fa-google-plus"></i></a></li>
-					<li><a class="linkedin" href="portfolio-4col.html#"><i class="fa fa-linkedin"></i></a></li>
-					<li><a class="pinterest" href="portfolio-4col.html#"><i class="fa fa-pinterest"></i></a></li>
-				</ul>
-			</div>
-			<div class="up-footer">
-				<div class="container">
-					<div class="row">
-
-						<div class="col-md-3">
-							<div class="widget footer-widget text-widget">
-								<h1>About</h1>
-								<p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
-								<p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. </p>
-								<img src="images/footer-logo.png" alt="">
-							</div>
-						</div>
-
-						<div class="col-md-3">
-							<div class="widget footer-widget tweets-widget">
-								<h1>Recent Tweets</h1>
-								<ul>
-									<li>
-										<i class="fa fa-twitter"></i>
-										<p>Photoshop to CSS3 magic - CSS Hat - <a href="portfolio-4col.html#">http://t.co/ad2CggDN</a> </p>
-										<span>15 June 2012</span>
-									</li>
-									<li>
-										<i class="fa fa-twitter"></i>
-										<p>CrossFit Advanced - another well done website, featured on CSSLuxury -  <a href="portfolio-4col.html#">http://t.co/ad2CggDN</a> </p>
-										<span>15 June 2012</span>
-									</li>
-								</ul>
-							</div>
-						</div>
-
-						<div class="col-md-3">
-							<div class="widget footer-widget flickr-widget">
-								<h1>Flickr Stream</h1>
-								<ul class="flickr">
-									<li><a href="portfolio-4col.html#"><img alt="" src="upload/flickr1.jpg"></a></li>
-									<li><a href="portfolio-4col.html#"><img alt="" src="upload/flickr2.jpg"></a></li>
-									<li><a href="portfolio-4col.html#"><img alt="" src="upload/flickr3.jpg"></a></li>
-									<li><a href="portfolio-4col.html#"><img alt="" src="upload/flickr4.jpg"></a></li>
-									<li><a href="portfolio-4col.html#"><img alt="" src="upload/flickr5.jpg"></a></li>
-									<li><a href="portfolio-4col.html#"><img alt="" src="upload/flickr6.jpg"></a></li>
-									<li><a href="portfolio-4col.html#"><img alt="" src="upload/flickr2.jpg"></a></li>
-									<li><a href="portfolio-4col.html#"><img alt="" src="upload/flickr3.jpg"></a></li>
-									<li><a href="portfolio-4col.html#"><img alt="" src="upload/flickr1.jpg"></a></li>
-								</ul>
-							</div>							
-						</div>
-
-						<div class="col-md-3">
-							<div class="widget footer-widget subscribe-widget">
-								<h1>Subscribe</h1>
-								<p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. </p>
-								<form class="subscribe-form">
-									<input type="text" id="e-mail" name="e-mail" placeholder="Email Address"/>
-									<input type="submit" id="submit" value="submit"/>
-								</form>
-							</div>							
-						</div>
-
-					</div>
-					<div class="footer-line">
-						<p>Copyright 2014. "marble" by UpiFix. All rights reserved.</p>
-					</div>
-				</div>
-			</div>
-
-		</footer>
-		<!-- End footer -->
-	</div>
-	<!-- End Container -->
-	
-	<script type="text/javascript" src="js/jquery.min.js"></script>
-	<script type="text/javascript" src="js/jquery.migrate.js"></script>
-	<script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
-	<script type="text/javascript" src="js/jquery.bxslider.min.js"></script>
-	<script type="text/javascript" src="js/jquery.appear.js"></script>
-	<script type="text/javascript" src="js/jquery.stellar.min.js"></script>
-	<script type="text/javascript" src="js/bootstrap.js"></script>
-	<script type="text/javascript" src="js/jquery.imagesloaded.min.js"></script>
-	<script type="text/javascript" src="js/jquery.isotope.min.js"></script>
-	<script type="text/javascript" src="js/retina-1.1.0.min.js"></script>
-	<script type="text/javascript" src="js/plugins-scroll.js"></script>
-	<script type="text/javascript" src="js/waypoint.min.js"></script>
-	<script type="text/javascript" src="js/script.js"></script>
-
-	
-</body>
-</html>
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/jquery.migrate.js"></script>
+		<script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
+		<script type="text/javascript" src="js/jquery.bxslider.min.js"></script>
+		<script type="text/javascript" src="js/jquery.appear.js"></script>
+		<script type="text/javascript" src="js/jquery.stellar.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.js"></script>
+		<script type="text/javascript" src="js/jquery.imagesloaded.min.js"></script>
+		<script type="text/javascript" src="js/jquery.isotope.min.js"></script>
+		<script type="text/javascript" src="js/retina-1.1.0.min.js"></script>
+		<script type="text/javascript" src="js/plugins-scroll.js"></script>
+		<script type="text/javascript" src="js/waypoint.min.js"></script>
+		<script type="text/javascript" src="js/script.js"></script>
