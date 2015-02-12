f17371937352db09069cc11dd884195cd2a6eaf1
Fllotouch <Floca@MacBook-Air-de-Florent.local>
2015-02-12 17:44:58 +0100

    Correction

--- public/index.php
+++ public/index.php
@@ -1,18 +1,21 @@
 <?php
-    require '../app/Autoloader.php';
+    require '../app/Autoloader.php'; //Charge l'autoloader pour les futures class
 
-    App\Autoloader::register();
+    App\Autoloader::register(); //
 
-    if (isset($_GET['p']))
+    if (isset($_GET['p'])) //On récupére la variable ?p=...
         $p = $_GET['p'];
     else
         $p = "home";
-    ob_start();
+
+    ob_start(); //on va enregistrer tout ce qui va etre affiché à partir de maintenant
+
     if($p === "home")
         require '../pages/home.php';
     elseif($p === "biere")
         require '../pages/biere.php';
 
-    $content = ob_get_clean();
-    require '../pages/template/template.php'
+    $content = ob_get_clean(); //On met tout dans la variable content et on nettoie l'écran
+
+    require '../pages/template/template.php'; //et on affiche le canva de base
 ?>
