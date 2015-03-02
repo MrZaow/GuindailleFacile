74ecb6d074c566cf677d0fcb39b2d9b5990f5a1d
Samyn Axel <brinx@live.be>
2015-03-02 14:17:59 +0100

    Modification HTML
    J'ai ajouté des balise du style col-*-* pour mieux gérer la responsivité

--- pages/template/template.php
+++ pages/template/template.php
@@ -35,38 +35,43 @@
         <!-- Static navbar -->
         <div class="navbar navbar-default navbar-fixed-top">
             <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="?p=home"><img alt="" src="images/logo8.png"></a>
-                </div>
-                <div class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav navbar-right">
-
-                        <li><a class="active" href="index.php">Accueil</a></li>
-                        <li><a href="?p=etudiant">Coin étudiant</a></li>
-                        <li><a href="#">Alcools</a>
-                            <ul class="drop-down">
-                                <li><a href="?p=cocktail">Cocktails</a></li>
-                                <li><a href="?p=biere">Bières</a></li>
-                                <li><a href="?p=fort">Alcools forts</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="index.php?p=jeux">Jeux d'alcool</a></li>
-                        <li><a href="index.php?p=limite">Connaitre ses limites</a></li>
-                        <li class="drop"><a href="#" class="open-search"><i class="fa fa-search"></i></a>
-                            <form class="form-search">
-                                <input type="search" placeholder="Chercher un alcool"/>
-                                <button type="submit">
-                                    <i class="fa fa-search"></i>
-                                </button>
-                            </form>
-                        </li>
-                    </ul>
-                </div>
+				<div class="row">
+					<div class="col-lg-3 col-md-3 col-sm-3">
+						<div class="navbar-heade">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="?p=home"><img alt="" src="images/logo8.png"></a>
+						</div>
+					</div>
+					<div class="col-lg-9 col-md-9 col-sm-9">
+						<div class="navbar-collapse collapse">
+							<ul class="nav navbar-nav navbar-right">
+								<li><a class="active" href="index.php">Accueil</a></li>
+								<li><a href="?p=etudiant">Coin étudiant</a></li>
+								<li><a href="#">Alcools</a>
+									<ul class="drop-down">
+										<li><a href="?p=cocktail">Cocktails</a></li>
+										<li><a href="?p=biere">Bières</a></li>
+										<li><a href="?p=fort">Alcools forts</a></li>
+									</ul>
+								</li>
+								<li><a href="index.php?p=jeux">Jeux d'alcool</a></li>
+								<li><a href="index.php?p=limite">Connaitre ses limites</a></li>
+								<li class="drop"><a href="#" class="open-search"><i class="fa fa-search"></i></a>
+									<form class="form-search">
+										<input type="search" placeholder="Chercher un alcool"/>
+										<button type="submit">
+											<i class="fa fa-search"></i>
+										</button>
+									</form>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
             </div>
         </div>
     </header>
