b09ae8d6d2bd37436d5a8e85260d88bc85dc1af3
MrZaow <misterzaow@gmail.com>
2015-03-10 17:01:09 +0100

    Mise à jour encodage

--- pages/encodage.php
+++ pages/encodage.php
@@ -5,6 +5,9 @@ if(isset($_POST["submit"]))
     $arg = array($_POST["nom"], $_POST["resume"], $_POST["description"], $_POST["mesure"]);
     App\App::getDB()->prepare("INSERT INTO ingredients (nom, resume, description, unitemesure) VALUES (?, ?, ?, ?)",$arg ,'', true, false);
 
+    $arg = array($_POST["pourcentagealcool"], $_POST["prixlitre"], $_POST["cotesur5"], $_POST['image1'], $_POST['image2'], $_POST['image3']);
+    App\App::getDB()->prepare("INSERT INTO boissons (pourcentagealcool, prixlitre, cotesur5, image1, image2, image3, idingredient) VALUES (?, ?, ?, ?, ?, ?, (SELECT idingredient FROM ingredients ORDER BY idingredient DESC LIMIT 1))",$arg ,'', true, false);
+
     switch($_POST["categorie"])
     {
         case 'biere':
@@ -35,12 +38,44 @@ if(isset($_POST["submit"]))
     <div class="section-content">
         <div class="container">
             <div class="row">
-                <div class="col-md-6">
-                    <h1>Bière</h1>
+                <div class="col-md-4">
+                    <h1>Ingrédient</h1>
                     <form action="?p=encodage" method="post">
                         <div class="form-group">
                             <label>Nom</label>
                             <input type="text" class="form-control" name="nom" placeholder="Nom" autofocus>
+                        </div>
+                        <div class="form-group">
+                            <label>Résumé</label><br>
+                            <textarea cols="120" placeholder="Le résumé" name="resume" class="form-control"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label>Description</label><br>
+                            <textarea col="120" placeholder="La description" name="description" class="form-control"></textarea>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Unité de mesure</label><br>
+                            <select class="form-control" name="mesure">
+                                <option>l</option>
+                                <option>cl</option>
+                                <option>gr</option>
+                                <option>pincées</option>
+                                <option>cuillère à café</option>
+                                <option>cuillère à soupe</option>
+                            </select>
+                        </div>
+                        <input type="hidden" name="categorie" value="simple">
+                        <input type="submit" class="btn btn-primary" name="submit" value="Envoyer"><br><br>
+                    </form>
+                </div>
+
+                <div class="col-md-4">
+                    <h1>Bière</h1>
+                    <form action="?p=encodage" method="post">
+                        <div class="form-group">
+                            <label>Nom</label>
+                            <input type="text" class="form-control" name="nom" placeholder="Nom" >
                         </div>
                         <div class="form-group">
                             <label>Résumé</label><br>
@@ -70,44 +105,40 @@ if(isset($_POST["submit"]))
                                 <option>gr</option>
                             </select>
                         </div>
+                        <div class="form-group">
+                            <label>Pourcentage d'alcool</label><br>
+                            <input type="text" class="form-control" name="pourcentagealcool" placeholder="Pourcentage d'alcool">
+                        </div>
+                        <div class="form-group">
+                            <label>Prix au litre</label><br>
+                            <input type="text" class="form-control" name="prixlitre" placeholder="Prix au litre">
+                        </div>
+                        <div class="form-group">
+                            <label>Cote sur 5</label><br>
+                            <input type="text" class="form-control" name="cotesur5" placeholder="Cote sur 5">
+                        </div>
+                        <div class="form-group">
+                            <label>Image 1</label><br>
+                            <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
+                            <input type="file" class="form-control" name="image1" placeholder="Image 1 (max 1 Mo)">
+                        </div>
+                        <div class="form-group">
+                            <label>Image 2</label><br>
+                            <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
+                            <input type="file" class="form-control" name="image2" placeholder="Image 2 (max 1 Mo)">
+                        </div>
+                        <div class="form-group">
+                            <label>Image 3</label><br>
+                            <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
+                            <input type="file" class="form-control" name="image3" placeholder="Image 3 (max 1 Mo)">
+                        </div>
+
                         <input type="hidden" name="categorie" value="biere">
                         <input type="submit" class="btn btn-primary" name="submit" value="Envoyer"><br><br>
                     </form>
                 </div>
-                <div class="col-md-6">
-                    <h1>Ingrédient</h1>
-                    <form action="?p=encodage" method="post">
-                        <div class="form-group">
-                            <label>Nom</label>
-                            <input type="text" class="form-control" name="nom" placeholder="Nom">
-                        </div>
-                        <div class="form-group">
-                            <label>Résumé</label><br>
-                            <textarea cols="120" placeholder="Le résumé" name="resume" class="form-control"></textarea>
-                        </div>
-                        <div class="form-group">
-                            <label>Description</label><br>
-                            <textarea col="120" placeholder="La description" name="description" class="form-control"></textarea>
-                        </div>
-
-                        <div class="form-group">
-                            <label>Unité de mesure</label><br>
-                            <select class="form-control" name="mesure">
-                                <option>l</option>
-                                <option>cl</option>
-                                <option>gr</option>
-                                <option>pincées</option>
-                                <option>cuillère à café</option>
-                                <option>cuillère à soupe</option>
-                            </select>
-                        </div>
-                        <input type="hidden" name="categorie" value="simple">
-                        <input type="submit" class="btn btn-primary" name="submit" value="Envoyer"><br><br>
-                    </form>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-6">
+                
+                <div class="col-md-4">
                     <h1>Alcool fort</h1>
                     <form action="?p=encodage" method="post">
                         <div class="form-group">
@@ -131,11 +162,37 @@ if(isset($_POST["submit"]))
                                 <option>gr</option>
                             </select>
                         </div>
+                        <div class="form-group">
+                            <label>Pourcentage d'alcool</label><br>
+                            <input type="text" class="form-control" name="pourcentagealcool" placeholder="Pourcentage d'alcool">
+                        </div>
+                        <div class="form-group">
+                            <label>Prix au litre</label><br>
+                            <input type="text" class="form-control" name="prixlitre" placeholder="Prix au litre">
+                        </div>
+                        <div class="form-group">
+                            <label>Cote sur 5</label><br>
+                            <input type="text" class="form-control" name="cotesur5" placeholder="Cote sur 5">
+                        </div>
+                        <div class="form-group">
+                            <label>Image 1</label><br>
+                            <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
+                            <input type="file" class="form-control" name="image1" placeholder="Image 1 (max 1 Mo)">
+                        </div>
+                        <div class="form-group">
+                            <label>Image 2</label><br>
+                            <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
+                            <input type="file" class="form-control" name="image2" placeholder="Image 2 (max 1 Mo)">
+                        </div>
+                        <div class="form-group">
+                            <label>Image 3</label><br>
+                            <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
+                            <input type="file" class="form-control" name="image3" placeholder="Image 3 (max 1 Mo)">
+                        </div>
                         <input type="hidden" name="categorie" value="alcoolfort">
                         <input type="submit" class="btn btn-primary" name="submit" value="Envoyer"><br><br>
                     </form>
                 </div>
-
             </div>
         </div>
     </div>
