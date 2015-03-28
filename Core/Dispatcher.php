9be5b102d4b0e03bf3d8c49882fa026b729e4853
Jeremy Smith <contact@swith.fr>
2015-03-28 17:56:18 +0100

    Add process to dispatcher

--- Core/Dispatcher.php
+++ Core/Dispatcher.php
@@ -16,9 +16,17 @@ class Dispatcher
 		$route = $this->Request->controller . '/' . $this->Request->action;
 
 		if (in_array($route, include(ROUTES))) {
-			die("yes");
+            $controllerName = 'App\\Controllers\\' . ucfirst($this->Request->controller) . 'Controller';
+			$controller = new $controllerName;
+
+            extract(call_user_func([$controller,$this->Request->action]));
+
+            ob_start();
+            require('./App/Views/' . ucfirst($this->Request->controller) . '/' . $controller->view . '.php');
+            $layout_content = ob_get_clean();
+            require('./App/Views/Layouts/' . $controller->layout . '.php');
 		} else {
-			die("no");
+			die("Page introuvable");
 		}
 
 	}
