b148f83c76f12219380334262dde235cbcf52491
Jeremy Smith <contact@swith.fr>
2015-03-30 19:35:00 +0200

    add database configs

--- App/Configs/database.php
+++ App/Configs/database.php
@@ -1,5 +1,12 @@
-<?php 
+<?php
 
 return [
-
+    'host'     => 'localhost',
+    'dbName'   => 'gindaillefacile',
+    'username' => 'root',
+    'password' => 'root',
+    'options'  => [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]
 ];
