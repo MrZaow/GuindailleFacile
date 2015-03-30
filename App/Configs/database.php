1d796989f885b85756212e128dfb6b27c963d00d
MrZaow <misterzaow@gmail.com>
2015-03-30 20:10:07 +0200

    Revert "Bonne dbname"
    This reverts commit 8713e0e678d76217c987b0d15abad2ed21d47fee.

--- App/Configs/database.php
+++ App/Configs/database.php
@@ -2,7 +2,7 @@
 
 return [
     'host'     => 'localhost',
-    'dbName'   => 'guindaillefacile',
+    'dbName'   => 'gindaillefacile',
     'username' => 'root',
     'password' => 'root',
     'options'  => [
