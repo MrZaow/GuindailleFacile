cc072dc7b6664a0a7ae4a4f93dcbf25a5d965d26
Jeremy Smith <contact@swith.fr>
2015-03-30 19:35:14 +0200

    add function loadModel

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -7,5 +7,9 @@ use Core\Controller;
 
 class AppController extends Controller
 {
-
+    public function loadModel($modelName)
+    {
+        $model = 'App\\Models\\' . $modelName;
+        $this->$modelName = new $model();
+    }
 } 
