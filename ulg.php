343478a80f82982b6fa1fbdef6c6dd45fe2f5c8a
Fllotouch <Floca@MacBook-Air-de-Florent.local>
2015-05-16 18:37:56 +0200

    Ajout de l'iframe
    UlgConfession et maj bootstrap

---
+++ ulg.php
@@ -0,0 +1,49 @@
+<?php include("includes/connectionpdo.php");?>
+<!doctype html>
+
+<html lang="fr" class="no-js">
+
+<?php include("includes/head.php") ?>
+
+<body>
+<!-- Container -->
+<div id="container">
+
+    <?php include("includes/header.php") ?>
+
+    <div id="content">
+
+        <!-- page-banner
+            ================================================== -->
+        <div class="section-content page-banner blog-page-banner">
+            <div class="container">
+                <h1>Le coin guindaille</h1>
+            </div>
+        </div>
+
+        <!-- blog-section
+            ================================================== -->
+        <div class="section-content blog-section with-sidebar">
+            <div class="container">
+                <div class="blog-box">
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="http://floca.be/ulg" height="300000"></iframe>
+                    </div>
+
+                </div>
+            </div>
+            <a class="go-top" href="blog-rightsidebar.html#"><i class="fa fa-arrow-circle-o-up"></i></a>
+        </div>
+
+
+    </div>
+    <!-- content
+                ================================================== -->
+    <?php include("includes/footer.php") ?>
+
+</div>
+<!-- End Container -->
+<?php include("includes/script.php") ?>
+
+</body>
+</html>
